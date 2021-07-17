691820864961169ea4076259ccddd8953c5140c0
2671467850 <wy2671467850@163.com>
2021-07-17 19:26:11 +0800

    add-xmly-weapplet&test-audio

--- myweapplet/pages/index/index.js
+++ myweapplet/pages/index/index.js
@@ -1,4 +1,5 @@
 // pages/index/index.js
+const myaudio = wx.createInnerAudioContext({});
 Page({
 
      /**
@@ -8,11 +9,34 @@ Page({
 
      },
 
+     
+  //播放
+  play: function () {
+      
+     myaudio.play();
+     console.log(myaudio.duration);
+     this.setData({ isplay: true });
+   },
+   // 停止
+   stop: function () {
+     myaudio.pause();
+     this.setData({ isplay: false });
+   },
+   /**
+    * 重播
+    */
+   review:function(){
+    myaudio.stop();
+  
+     myaudio.play()
+     this.setData({ isplay: true });
+   },
+
      /**
       * 生命周期函数--监听页面加载
       */
      onLoad: function (options) {
-
+          
      },
 
      /**
@@ -26,7 +50,7 @@ Page({
       * 生命周期函数--监听页面显示
       */
      onShow: function () {
-
+          myaudio.src = "https://aod.cos.tx.xmcdn.com/group31/M0A/21/AD/wKgJSVmBku_ANXULAOBNF_Mwy-o627.m4a"
      },
 
      /**
